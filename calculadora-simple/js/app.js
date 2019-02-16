d338a2b999b2237f71bf5500abdd5b9fcc7e308f
Cropierr <cesar.medva@gmail.com>
2019-02-15 20:37:29 -0600

    cambios calculadora

--- calculadora-simple/js/app.js
+++ calculadora-simple/js/app.js
@@ -1,6 +1,25 @@
+function calcular (operadorA, operadorB, operacion) {
+  let result = 0
+  if (operacion === "suma") {
+      result = operadorA + operadorB
+  } else if (operacion === "resta") {
+      result = operadorA - operadorB
+  }
+  return result
+
+}
+
 window.addEventListener('load', function () {
+    const operadorA = document.getElementById('operadorA')
+    const operadorB = document.getElementById('operadorB')
+    const operacion = document.getElementById('operacion')
+
 const button = document.getElementById('calcular')
 button.addEventListener('click', function () {
-alert('Fui Clickeado')
-})
+    const operadorAValue = operadorA.Value
+    const operadorBValue = operadorB.value
+    const result = calcular(operadorAValue, operadorBValue, operacion.Value)
+    alert(`El resultado es ${result}`)
+
+   })
 })
